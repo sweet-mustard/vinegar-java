36896e7aa32e5ac4f1c0a3055274ebd4b62574c1
Stijn Van Bael <stijn.vanbael@sweetmustard.be>
2018-09-27 08:44:33 +0200

    Documented MaybeMatch

--- pattern-matcher/src/main/java/be/sweetmustard/vinegar/matcher/MaybeMatch.java
+++ pattern-matcher/src/main/java/be/sweetmustard/vinegar/matcher/MaybeMatch.java
@@ -3,26 +3,53 @@ package be.sweetmustard.vinegar.matcher;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
+/**
+ * A value that is a possible match of a {@link MappingCondition}.
+ *
+ * @param <T> The type of the object container by this match.
+ */
 public interface MaybeMatch<T> {
-    NoMatch<?> NO_MATCH = new NoMatch<>();
-
+    /**
+     * Returns whether or not this value matches the condition.
+     */
     boolean matches();
 
+    /**
+     * Returns the matching value, if the condition matches, otherwise null.
+     */
     T getValue();
 
+    /**
+     * Maps this value to another value, if it matches.
+     *
+     * @param mapper the mapper function to apply to the value.
+     * @param <U> the type to map to
+     * @return a new <code>MaybeMatch</code> containing the mapped value.
+     */
     <U> MaybeMatch<U> map(Function<? super T, ? extends U> mapper);
 
+    /**
+     * Creates a new <code>MaybeMatch</code>
+     * @param match whether or not it is a match.
+     * @param value A supplier that supplies the value, in case it is a match
+     */
     static <T> MaybeMatch<T> create(boolean match, Supplier<T> value) {
         return match ? match(value.get()) : noMatch();
     }
 
-    static <T> Match<T> match(T value) {
+    /**
+     * Creates a new <code>Match</code> with the specified value.
+     */
+    static <T> MaybeMatch<T> match(T value) {
         return new Match<>(value);
     }
 
+    /**
+     * Returns a <code>NoMatch</code>.
+     */
     @SuppressWarnings("unchecked")
-    static <T> NoMatch<T> noMatch() {
-        return (NoMatch<T>) NO_MATCH;
+    static <T> MaybeMatch<T> noMatch() {
+        return (MaybeMatch<T>) NoMatch.INSTANCE;
     }
 
     final class Match<T> implements MaybeMatch<T> {
@@ -49,9 +76,8 @@ public interface MaybeMatch<T> {
         }
     }
 
-    final class NoMatch<T> implements MaybeMatch<T> {
-        NoMatch() {
-        }
+    enum NoMatch implements MaybeMatch<Object> {
+        INSTANCE;
 
         @Override
         public boolean matches() {
@@ -59,14 +85,15 @@ public interface MaybeMatch<T> {
         }
 
         @Override
-        public T getValue() {
+        public Object getValue() {
             return null;
         }
 
         @SuppressWarnings("unchecked")
         @Override
-        public <U> MaybeMatch<U> map(final Function<? super T, ? extends U> mapper) {
-            return (MaybeMatch<U>) NO_MATCH;
+        public <U> MaybeMatch<U> map(final Function<? super Object, ? extends U> mapper) {
+            return (MaybeMatch<U>) this;
         }
+
     }
 }
